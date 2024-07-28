836962115edd57768198c4bb8a2bb4d4c404344d
CESCO <cescovalledev@gmail.com>
2024-07-27 20:38:30 -0600

    next auth uninstalled

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,8 @@
 import { NextPage } from "next";
-import { getSession } from "next-auth/react";
-import { redirect } from "next/navigation";
 
 interface Props {}
 
 const Page: NextPage<Props> = async ({}) => {
-  const session = await getSession();
-
-  if (!session) {
-    redirect("/login");
-  }
-
   return <div>Welcome to Dashboard</div>;
 };
 
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,8 @@
 import { NextPage } from "next";
-import { getSession } from "next-auth/react";
-import { redirect } from "next/navigation";
 
 interface Props {}
 
 const Page: NextPage<Props> = async ({}) => {
-  const session = await getSession();
-
-  if (session) {
-    redirect("/dashboard");
-  }
   return (
     <div>
       <h1>Login Page</h1>
